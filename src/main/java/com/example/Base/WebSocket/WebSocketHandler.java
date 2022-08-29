05a3ac45ccce4ad3d8401e7935f7c0231e509f4e
DESKTOP-TFBFNVC\BIT <moonjinyoung.dev@gmail.com>
2022-08-29 20:04:29 +0900

    Update

--- src/main/java/com/example/Base/WebSocket/WebSocketHandler.java
+++ src/main/java/com/example/Base/WebSocket/WebSocketHandler.java
@@ -4,12 +4,15 @@ import com.example.Base.domain.dto.user.UserDTO;
 import com.example.Base.service.token.TokenServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,30 +20,30 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
 
-    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
-    private final TokenServiceImpl tokenService;
-    private final HttpServletRequest request;
+    //현재 접속한 모든 사용자, 전체 알림 을 위하여 혹은 해당 카테고리 설정한 전문가에게 전부 알림
+    List<WebSocketSession> sessions = new ArrayList<>();
+
+    // 1 ㄷ 1 알림 을 위한 Map
+    private final Map<String, WebSocketSession> sessionsMap = new ConcurrentHashMap<>();
 
     @Override //웹소캣 연결 시
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        UserDTO user = tokenService.decodeJWT(request);
-        String email = user.getEmail();
-        log.info(email);
+        sessions.add(session); //세션 저장
 
-        sessions.put(email, session); //세션 저장
-        log.info(sessions);
+        String email = getEmail(session);
+        sessionsMap.put(email,session); //로그인중인 개별유저
     }
 
     //양방향 데이터 송신
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        log.info("session : " + session);
-        log.info("content : " + message);
 
-        TextMessage test = new TextMessage("aaa"+"aaa");
-        String sender = session.getId();
-
-        log.info(sender);
+        String msg = message.getPayload();
+        if ()
+        for (WebSocketSession sess: sessions){
+            sess.sendMessage(new TextMessage(email + "님이 알림을 보냄"));
+        }
+        log.info(email);
     }
 
     //소캣 연결 종료
@@ -48,7 +51,8 @@ public class WebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         String sessionID = session.getId();
 
-        sessions.remove(sessionID);
+        sessions.remove(session);
+        sessionsMap.remove(sessionID);
     }
 
     //소캣 통신 에러
@@ -56,8 +60,21 @@ public class WebSocketHandler extends TextWebSocketHandler {
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
         String sessionID = session.getId();
 
-        sessions.remove(sessionID);
+        sessions.remove(session);
+        sessionsMap.remove(sessionID);
     }
+
+    private String getEmail(WebSocketSession session) {
+        Map<String, Object> httpSession = session.getAttributes();
+        UserDTO loginUser = (UserDTO) httpSession.get("email");
+
+        if(loginUser == null) {
+            return session.getId();
+        } else {
+            return loginUser.getEmail();
+        }
+    }
+
 
     //로그인 성공시 혹은 로그인 철와동시에 session구독 처리 한다.
     // 개별 서비스 로직에서 알림이 필요한 서비스 구동시 처리 후 확인 되면 알림 메소드를 사용하여 특정 session구독자들 모아둔 곳에서 보내고자하는 session을 찾아서 그 새션에 보낸다
--- src/main/java/com/example/Base/WebSocket/config/WebSocketConfig.java
+++ src/main/java/com/example/Base/WebSocket/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.example.Base.WebSocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws"); //연결 시작점
+        registry.addEndpoint("/ws"); //연결 시작점 "ws://localhost:8000/....."
         registry.addEndpoint("/ws").withSockJS(); //socketjs 사용
     }
 
